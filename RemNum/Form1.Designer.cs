96d28fd2d22767fdeb68ebc2e6383a222bb230e8
Vadershik <gogofight2k20@gmail.com>
2021-11-05 16:45:01 +0300

    1.0

---
+++ RemNum/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace RemNum
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.recordText = new System.Windows.Forms.Label();
+            this.NumText = new System.Windows.Forms.Label();
+            this.PrintNum = new System.Windows.Forms.TextBox();
+            this.CheckBut = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // recordText
+            // 
+            this.recordText.AutoSize = true;
+            this.recordText.Location = new System.Drawing.Point(276, 9);
+            this.recordText.Name = "recordText";
+            this.recordText.Size = new System.Drawing.Size(56, 13);
+            this.recordText.TabIndex = 0;
+            this.recordText.Text = "Рекорд: 0";
+            // 
+            // NumText
+            // 
+            this.NumText.AutoSize = true;
+            this.NumText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.NumText.Location = new System.Drawing.Point(143, 44);
+            this.NumText.Name = "NumText";
+            this.NumText.Size = new System.Drawing.Size(63, 24);
+            this.NumText.TabIndex = 1;
+            this.NumText.Text = "Число";
+            // 
+            // PrintNum
+            // 
+            this.PrintNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PrintNum.Location = new System.Drawing.Point(96, 90);
+            this.PrintNum.Name = "PrintNum";
+            this.PrintNum.Size = new System.Drawing.Size(163, 22);
+            this.PrintNum.TabIndex = 2;
+            // 
+            // CheckBut
+            // 
+            this.CheckBut.Location = new System.Drawing.Point(138, 129);
+            this.CheckBut.Name = "CheckBut";
+            this.CheckBut.Size = new System.Drawing.Size(75, 23);
+            this.CheckBut.TabIndex = 3;
+            this.CheckBut.Text = "Check";
+            this.CheckBut.UseVisualStyleBackColor = true;
+            this.CheckBut.Click += new System.EventHandler(this.CheckBut_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 187);
+            this.Controls.Add(this.CheckBut);
+            this.Controls.Add(this.PrintNum);
+            this.Controls.Add(this.NumText);
+            this.Controls.Add(this.recordText);
+            this.Name = "Form1";
+            this.Text = "RemNum";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label recordText;
+        private System.Windows.Forms.Label NumText;
+        private System.Windows.Forms.TextBox PrintNum;
+        private System.Windows.Forms.Button CheckBut;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ RemNum/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace RemNum
+{
+    public partial class Form1 : Form
+    {
+        public int timeLeft = 5;
+        int rec = 1;
+        int start = 1, end = 9;
+        Random gennum = new Random();
+        int num = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 1000;
+        }
+
+        private void CheckBut_Click(object sender, EventArgs e)
+        {
+            string numtext = NumText.Text;
+            num = gennum.Next(start*rec,end*rec);
+            timeLeft = 5;
+            timer1.Start();
+            NumText.Text = Convert.ToString(num);
+            PrintNum.Visible = false;
+            timeLeft = 5;
+            timer1.Start();
+            PrintNum.Visible = true;
+            Thread.Sleep(timeLeft * 1000);
+            NumText.Text = "";
+            timeLeft = 5;
+            timer1.Start();
+            int pnum = PrintNum.Text=="" ? 0 : Convert.ToInt32(PrintNum.Text);
+            if (num == pnum)
+            {
+                NumText.Text = "True";
+                rec += 1;
+            }
+            else
+            {
+                NumText.Text = "False";
+                rec = 1;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(timeLeft > 0)
+            {
+                timeLeft = timeLeft - 1;
+            } else
+            {
+                timer1.Stop();
+            }
+        }
+    }
+}
